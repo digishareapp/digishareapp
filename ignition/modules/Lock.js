583d73fe02752eaa8b88f79c6fe3f52ec44436e9
digishareapp <digishare.app@gmail.com>
2024-05-21 17:16:59 +0000

    update Lock.js

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -2,6 +2,6 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("FileSharing", (m) => {
   const fileSharing = m.contract("FileSharingDApp");
-
+  // return contract address
   return { fileSharing };
 });
